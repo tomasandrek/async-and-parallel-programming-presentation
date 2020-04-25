4f50cf615901bd7df02974ec517ba32f16b8f72a
Tomas Andrek <tomas.andrek@gmail.com>
2020-04-25 09:35:23 +0200

    Added client code

---
+++ ClientDemonstration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClientDemonstration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+
+        public string Example1()
+        {
+            return "asdwe";
+        }
+    }
+}
